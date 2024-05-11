393a5951227215043e51f0047ece60d2830fad92
Stefan <stefan.karlberg@proton.me>
2024-05-11 14:08:35 +0200

    Updated build script to make life easier

--- build.sh
+++ build.sh
@@ -1,62 +1,155 @@
 #!/bin/zsh
 
-# Very basic build script to help with buliding for iOS
-# TODO:
-# - Make it build for more platforms
-# - Copy results to a folder  
-
-# You probably need to put your iOS device ID here
-# To get a list of available id's run:
-# xcodebuild -scheme GameCenter -showdestinations
-DEVICE_ID=""
-#DEVICE_ID="dvtdevice-DVTiPhonePlaceholder-iphoneos:placeholder" # Use for "Any iOS Device" usually gives errors though
+# Basic build script to help with building extensions
 
 PROJECT=$1
-CONFIG=$2
-DESTINATION=$3
-
-if [[ ! $DEVICE_ID && ! $DESTINATION ]];
-then
-	echo "No DEVICE_ID and no destination override specified"
-	echo "Open build.sh and add your device id to DEVICE_ID"
-	echo "Or run script with ./build.sh <project> <config> <destination>"
-	echo "where <destination> is defined as: \"platform=iOS,arch=arm64,id=<your device id>\""
+if [[ ! $PROJECT ]]; then
+	echo 
+	echo "Syntax: build <project> <platform?> <config?>"
+	echo 
+	echo "<project> is the project folder to build. eg GameCenter"
+	echo "<platform> is the platform to build for"
+	echo "	Options: ios, macos & all. (Default: all)"
+	echo "<config> is the configuration to use"
+	echo "	Options: debug & release. (Default: release)"
 	exit 0
 fi
 
-# The project folder to build, like "GameCenter", "InAppPurchase", "Networking" etc
-if [[ ! $PROJECT ]];
-then
-	echo "Please specify a folder to build"
-	exit 0
+TARGET=$2
+if [[ ! $TARGET ]]; then
+	TARGET="all"
 fi
 
-# Available configs: debug or release
-if [[ ! $CONFIG ]];
-then
-	CONFIG="debug"
+CONFIG=$3
+if [[ ! $CONFIG ]]; then
+	CONFIG="release"
 fi
 
-# If you prefer to override the destination manually
-if [[ ! $DESTINATION ]];
-then
-	DESTINATION="platform=iOS,arch=arm64,id=$DEVICE_ID"
-fi
+BUILD_PATH=".build"
+CACHE_PATH=".cache"
 
-# Target output folder, similar to how "swift build" does it
-TARGET="arm64-apple-ios/$CONFIG"
+build_ios() {
+	echo "Building $1 iOS library..."
 
-# If you want to target a different sdk you can see which are available using
-# xcodebuild -scheme <project> -showsdks
+	if (( $+commands[xcbeautify] )); then
+		xcodebuild \
+			-workspace "$1/" \
+			-scheme "$1" \
+			-destination 'generic/platform=iOS' \
+			-derivedDataPath "$BUILD_PATH" \
+			-clonedSourcePackagesDirPath "$CACHE_PATH" \
+			-configuration Release \
+			-skipPackagePluginValidation | xcbeautify
+	else
+		xcodebuild \
+			-workspace "$1/" \
+			-scheme "$1" \
+			-destination 'generic/platform=iOS' \
+			-derivedDataPath "$BUILD_PATH" \
+			-clonedSourcePackagesDirPath "$CACHE_PATH" \
+			-configuration Release \
+			-skipPackagePluginValidation -quiet
+	fi
 
-echo "Building $PROJECT iOS library..."
-xcodebuild \
-	-workspace "$PROJECT/" \
-	-scheme $PROJECT \
-	-sdk iphoneos17.4 \
-	-derivedDataPath "$PROJECT/.build/$TARGET" \
-	-configuration $CONFIG \
-	-destination $DESTINATION \
-	-skipPackagePluginValidation -skipMacroValidation -quiet
+	echo "Copying iOS binaries"
+	productpath="$BUILD_PATH/Build/Products/$2-iphoneos/PackageFrameworks"
+	binarypath="bin/ios"
+	
+	if ! [[ -e "$binarypath" ]]; then
+		mkdir -p "$binarypath"
+	fi
 
-echo "Finished building to target: .build/$TARGET"
+	cp -af "$productpath/$1.framework" "$binarypath"
+	if ! [[ -e "$binarypath/SwiftGodot.framework" ]]; then
+		cp -af "$productpath/SwiftGodot.framework" "$binarypath"
+	fi
+	
+	echo "Finished building $1"
+}
+
+build_macos() {
+	echo "Building $1 macOS library..."
+
+	if (( $+commands[xcbeautify] )); then
+		swift build \
+			--package-path $1 \
+			--configuration $2 \
+			--triple arm64-apple-macos \
+			--scratch-path $BUILD_PATH \
+			--cache-path $CACHE_PATH | xcbeautify
+	else
+		swift build \
+			--package-path $1 \
+			--configuration $2 \
+			--triple arm64-apple-macos \
+			--scratch-path $BUILD_PATH \
+			--cache-path $CACHE_PATH
+	fi
+
+	echo "Copying macos binaries"
+	productpath="$BUILD_PATH/arm64-apple-macos/$2"
+	binarypath="bin/macos"
+
+	if ! [[ -e "$binarypath" ]]; then
+		mkdir -p "$binarypath"
+	fi
+
+	cp "$productpath/lib$1.dylib" "$binarypath"
+	if ! [[ -e "$binarypath/libSwiftGodot.dylib" ]]; then
+		cp -af "$productpath/libSwiftGodot.dylib" "$binarypath"
+	fi
+
+	echo "Finished building $1"
+}
+
+build_macos_xcode() {
+	echo "Building $1 macOS library..."
+
+	if (( $+commands[xcbeautify] )); then
+		xcodebuild \
+			-workspace "$1/" \
+			-scheme "$1" \
+			-destination 'generic/platform=macOS' \
+			-derivedDataPath "$BUILD_PATH" \
+			-clonedSourcePackagesDirPath "$CACHE_PATH" \
+			-configuration Release \
+			-skipPackagePluginValidation | xcbeautify
+	else
+		xcodebuild \
+			-workspace "$1/" \
+			-scheme "$1" \
+			-destination 'generic/platform=macOS' \
+			-derivedDataPath "$BUILD_PATH" \
+			-clonedSourcePackagesDirPath "$CACHE_PATH" \
+			-configuration Release \
+			-skipPackagePluginValidation -quiet
+	fi
+
+	echo "Copying binaries"
+	productpath="$BUILD_PATH/Build/Products/Release/PackageFrameworks"
+	binarypath="bin/macos"
+	
+	if ! [[ -e "$binarypath" ]]; then
+		mkdir -p "$binarypath"
+	fi
+
+	cp -af "$productpath/$1.framework/Versions/Current/$1" "$binarypath"
+	if ! [[ -e "$binarypath/SwiftGodot.framework" ]]; then
+		cp -af "$productpath/SwiftGodot.framework" "$binarypath"
+	fi
+
+	echo "Finished building $1"
+}
+
+build_libs() {
+	echo "$(tput bold)Building $1 $3 libraries for $2 platforms$(tput sgr0)"
+	if [[ $2 == "all" || $2 == "macos" ]]; then
+		build_macos "$1" "$3"
+	fi
+
+	if [[ $2 == "all" || $2 == "ios" ]]; then
+		build_ios "$1" "$3"
+	fi
+}
+
+build_libs "$PROJECT" $TARGET $CONFIG
