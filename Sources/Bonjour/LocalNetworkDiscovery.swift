df2382ca497a75986187fdba1be44f14acc8f90b
Stefan <stefan.karlberg@proton.me>
2025-02-11 17:11:50 +0100

    Only run Bonjour on the main queue

--- Sources/Bonjour/LocalNetworkDiscovery.swift
+++ Sources/Bonjour/LocalNetworkDiscovery.swift
@@ -35,7 +35,7 @@ class LocalNetworkDiscovery: RefCounted {
 	var discoveredDevices: [Int: (NWBrowser.Result)] = [:]
 
 	deinit {
-		stop()
+		self.stop()
 	}
 
 	/// Start looking for Bonjour devices on the local network.
@@ -44,17 +44,19 @@ class LocalNetworkDiscovery: RefCounted {
 	/// 	- typeDescriptor: A service descriptor used to discover a Bonjour service.
 	@Callable
 	func start(typeDescriptor: String) {
-		let descriptor: NWBrowser.Descriptor = NWBrowser.Descriptor.bonjourWithTXTRecord(
-			type: typeDescriptor,
-			domain: "local."
-		)
-		let browser: NWBrowser = NWBrowser(for: descriptor, using: .tcp)
+		DispatchQueue.main.async {
+			let descriptor: NWBrowser.Descriptor = NWBrowser.Descriptor.bonjourWithTXTRecord(
+				type: typeDescriptor,
+				domain: "local."
+			)
+			let browser: NWBrowser = NWBrowser(for: descriptor, using: .tcp)
 
-		browser.stateUpdateHandler = stateChanged(to:)
-		browser.browseResultsChangedHandler = resultsChanged
+			browser.stateUpdateHandler = self.stateChanged(to:)
+			browser.browseResultsChangedHandler = self.resultsChanged
 
-		browser.start(queue: DispatchQueue.global(qos: .userInitiated))
-		self.browser = browser
+			browser.start(queue: DispatchQueue.global(qos: .userInitiated))
+			self.browser = browser
+		}
 	}
 
 	/// Stop looking for Bonjour devices
@@ -78,68 +80,73 @@ class LocalNetworkDiscovery: RefCounted {
 	///		- hashValue: The hash value for the discovered bonjour service.
 	///		- onComplete: Callback with parameter: (error: Variant, address: Variant, port: Variant) -> (error: Int, address: String, port: Int)
 	func resolveEndpoint(hashValue: Int, onComplete: Callable) {
-		// This whole thing is unfortunately necessary since you can't resolve an endpoint to host:port
-		if let result: NWBrowser.Result = discoveredDevices[hashValue] {
-			let endpoint: NWEndpoint = result.endpoint
-			var port: Int = 0
-			switch result.metadata {
-			case .bonjour(let record):
-				port = Int(record.dictionary["port"] ?? "0") ?? 0
-			default:
-				break
-			}
-
-			let networkParams: NWParameters = NWParameters.tcp
-			networkParams.prohibitedInterfaceTypes = [.loopback]
-			networkParams.serviceClass = NWParameters.ServiceClass.responsiveData
-
-			let ip: NWProtocolIP.Options = networkParams.defaultProtocolStack.internetProtocol! as! NWProtocolIP.Options
-			ip.version = .v4
-
-			connection = NWConnection(to: endpoint, using: networkParams)
-			connection?.stateUpdateHandler = { state in
-				switch state {
-				case .ready:
-					if let innerEndpoint: NWEndpoint = self.connection?.currentPath?.remoteEndpoint,
-						case .hostPort(let host, let tempPort) = innerEndpoint
-					{
-						self.connection?.cancel()
-
-						var address_string: String = ""
-						switch host {
-						case .ipv4(let address):
-							address_string = self.ipAddressToString(address)
-							break
-						case .ipv6(let address):
-							if let ipv4Address = address.asIPv4 {
-								address_string = self.ipAddressToString(ipv4Address)
-							} else {
-								GD.pushError("[Bonjour] Failed to resolve endpoint: Got incompatible IPv6 address")
-								onComplete.callDeferred(
-									Variant(NetworkDiscoveryError.incompatibleIPV6Address.rawValue),
-									Variant(),
-									Variant()
-								)
-								return
-							}
-						default:
-							break
-						}
-
-						onComplete.callDeferred(Variant(OK), Variant(address_string), Variant(port))
-					}
+		DispatchQueue.main.async {
+			// This whole thing is unfortunately necessary since you can't resolve an endpoint to host:port
+			if let result: NWBrowser.Result = self.discoveredDevices[hashValue] {
+				GD.print("Resolving endpoint \(result.endpoint)...")
+				let endpoint: NWEndpoint = result.endpoint
+				var port: Int = 0
+				switch result.metadata {
+				case .bonjour(let record):
+					port = Int(record.dictionary["port"] ?? "0") ?? 0
 				default:
 					break
 				}
+
+				let networkParams: NWParameters = NWParameters.tcp
+				networkParams.prohibitedInterfaceTypes = [.loopback]
+				networkParams.serviceClass = NWParameters.ServiceClass.responsiveData
+
+				let ip: NWProtocolIP.Options =
+					networkParams.defaultProtocolStack.internetProtocol! as! NWProtocolIP.Options
+				ip.version = .v4
+
+				self.connection = NWConnection(to: endpoint, using: networkParams)
+				self.connection?.stateUpdateHandler = { state in
+					GD.print("Resolver state: \(state)")
+					switch state {
+					case .ready:
+						if let innerEndpoint: NWEndpoint = self.connection?.currentPath?.remoteEndpoint,
+							case .hostPort(let host, let tempPort) = innerEndpoint
+						{
+							self.connection?.cancel()
+
+							var address_string: String = ""
+							switch host {
+							case .ipv4(let address):
+								address_string = self.ipAddressToString(address)
+								break
+							case .ipv6(let address):
+								if let ipv4Address = address.asIPv4 {
+									address_string = self.ipAddressToString(ipv4Address)
+								} else {
+									GD.pushError("[Bonjour] Failed to resolve endpoint: Got incompatible IPv6 address")
+									onComplete.callDeferred(
+										Variant(NetworkDiscoveryError.incompatibleIPV6Address.rawValue),
+										Variant(),
+										Variant()
+									)
+									return
+								}
+							default:
+								break
+							}
+
+							onComplete.callDeferred(Variant(OK), Variant(address_string), Variant(port))
+						}
+					default:
+						break
+					}
+				}
+				self.connection?.start(queue: DispatchQueue.global(qos: .userInteractive))
+			} else {
+				GD.pushError("[Bonjour] Failed to resolve endpoint. Error: No endpoint corresponding to: \(hashValue)")
+				onComplete.callDeferred(
+					Variant(NetworkDiscoveryError.failedToResolveEndpoint.rawValue),
+					Variant(),
+					Variant()
+				)
 			}
-			connection?.start(queue: DispatchQueue.global(qos: .userInteractive))
-		} else {
-			GD.pushError("[Bonjour] Failed to resolve endpoint. Error: No endpoint corresponding to: \(hashValue)")
-			onComplete.callDeferred(
-				Variant(NetworkDiscoveryError.failedToResolveEndpoint.rawValue),
-				Variant(),
-				Variant()
-			)
 		}
 	}
 
@@ -155,69 +162,76 @@ class LocalNetworkDiscovery: RefCounted {
 	}
 
 	func resultsChanged(updated: Set<NWBrowser.Result>, changes: Set<NWBrowser.Result.Change>) {
-		for change: NWBrowser.Result.Change in changes {
-			switch change
-			{
-			case .added(let result):
-				var server_port: Int = 0
-
-				switch result.metadata
+		DispatchQueue.main.async {
+			for change: NWBrowser.Result.Change in changes {
+				switch change
 				{
-				case .bonjour(let record):
-					server_port = Int(record.dictionary["port"] ?? "0") ?? 0
-				default:
+				case .added(let result):
+					var server_port: Int = 0
+
+					switch result.metadata
+					{
+					case .bonjour(let record):
+						server_port = Int(record.dictionary["port"] ?? "0") ?? 0
+					default:
+						break
+					}
+
+					switch result.endpoint
+					{
+					case .service(let service):
+						self.emit(
+							signal: LocalNetworkDiscovery.deviceDiscovered,
+							service.name,
+							server_port,
+							result.hashValue
+						)
+					default:
+						break
+					}
+
+					self.discoveredDevices[result.hashValue] = result
+
+				case .removed(let result):
+					self.discoveredDevices.removeValue(forKey: result.hashValue)
+
+					switch result.endpoint
+					{
+					case .service(let service):
+						self.emit(signal: LocalNetworkDiscovery.deviceLost, service.name, result.hashValue)
+					default:
+						break
+					}
+
+				case .changed(let old, let new, flags: _):
+					var server_port: Int = 0
+					switch new.metadata
+					{
+					case .bonjour(let record):
+						server_port = Int(record.dictionary["port"] ?? "0") ?? 0
+					default:
+						break
+					}
+
+					switch new.endpoint
+					{
+					case .service(let service):
+						self.emit(
+							signal: LocalNetworkDiscovery.deviceUpdated,
+							service.name,
+							server_port,
+							old.hashValue,
+							new.hashValue
+						)
+					default:
+						break
+					}
+
+					self.discoveredDevices.removeValue(forKey: old.hashValue)
+					self.discoveredDevices[new.hashValue] = new
+				case .identical:
 					break
 				}
-
-				switch result.endpoint
-				{
-				case .service(let service):
-					emit(signal: LocalNetworkDiscovery.deviceDiscovered, service.name, server_port, result.hashValue)
-				default:
-					break
-				}
-
-				discoveredDevices[result.hashValue] = result
-
-			case .removed(let result):
-				discoveredDevices.removeValue(forKey: result.hashValue)
-
-				switch result.endpoint
-				{
-				case .service(let service):
-					emit(signal: LocalNetworkDiscovery.deviceLost, service.name, result.hashValue)
-				default:
-					break
-				}
-
-			case .changed(let old, let new, flags: _):
-				var server_port: Int = 0
-				switch new.metadata
-				{
-				case .bonjour(let record):
-					server_port = Int(record.dictionary["port"] ?? "0") ?? 0
-				default:
-					break
-				}
-
-				switch new.endpoint
-				{
-				case .service(let service):
-					emit(
-						signal: LocalNetworkDiscovery.deviceUpdated,
-						service.name,
-						server_port,
-						old.hashValue,
-						new.hashValue
-					)
-				default:
-					break
-				}
-
-				discoveredDevices.removeValue(forKey: old.hashValue)
-				discoveredDevices[new.hashValue] = new
-			case .identical:
-				break
 			}
 		}
 	}
